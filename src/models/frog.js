b4ff39b2a1aa87d705aa0cb85baefa7fe319e643
jamesperry-github <crimsonperry.github@gmail.com>
2022-07-20 19:51:12 -0500

    added models folder/frog component

---
+++ src/models/frog.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+export default function Frog() {
+  const [test, setTest] = useState(false);
+
+  return (
+    <div className="frog">
+
+    </div>
+  );
+}
